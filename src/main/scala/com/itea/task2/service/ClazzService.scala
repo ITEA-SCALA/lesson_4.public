c81d864f9edadcc17491e4eb58aead50ed1c9a4d
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2021-10-07 16:39:09 +0300

    #master

--- src/main/scala/com/itea/task2/service/ClazzService.scala
+++ src/main/scala/com/itea/task2/service/ClazzService.scala
@@ -6,6 +6,8 @@ import com.itea.task2.service.mapper._
 
 class ClazzService(dao: ClazzDao) {
 
-  def get(): ClazzDto = ClazzToClazzDto( dao.get() )
-  def save(clazz: ClazzDto): Unit = dao.save( ClazzDtoToClazz(clazz) )
+  def get: ClazzDto = ClazzToClazzDto( dao.get )
+  def save(clazz: ClazzDto) = {
+    dao.save( ClazzDtoToClazz(clazz) )
+  }
 }
